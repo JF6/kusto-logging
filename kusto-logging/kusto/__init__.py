b6ecb49e7d4a5ff90e5aacf41ee22f0dca2dbdd7
jf6 <jfsix@microsoft.com>
2021-02-07 00:04:05 +0100

    namespace

--- kusto-logging/kusto/__init__.py
+++ kusto-logging/kusto/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License
 """Init file for azure namespace. https://github.com/Azure/azure-sdk-for-python/wiki/Azure-packaging"""
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+#__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+pkg_resources.declare_namespace(__name__)
